541f7ea87cc6de2e6b981203bc56ba9ac987d973
jinn <alexandre.Quilan.delaistre@gmail.com>
2022-09-22 00:29:49 +0200

    ✨ Controllers

---
+++ src/Controller/EventController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/event', name: 'app_event')]
+    public function index(): JsonResponse
+    {
+        //Fais en sorte que ton controller renvoies des données de ta BDD
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,18 +4,33 @@ namespace App\DataFixtures;
 use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
+    public function __construct(){
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
         //Ici je manipules des données pour creer mon Event.
         //Les differentes références sont obtensible ici : ../Entity/Event.php
         for ($i = 0; $i < 5; $i++) {
             $event = new Event;
-            $event->setEventName("Nom pas très original.");
-            $event->setEventDesc('Trouves mieux que ça serieux.');
-            $event->setEventStartDate(new \DateTime('09/01/2019'));
+            $endDate = $this->faker->optional($weight = 0.25)->dateTime($max = 'now');
+            $event->setEventName($this->faker->realText($maxNbChars =140, $indexSize = 2))
+            ->setEventDesc($this->faker->text())
+            ->setEventPrice($this->faker->optional($weight = 0.25)->randomDigit())
+            ->setEventStartDate($this->faker->dateTime($max = $endDate ? $endDate : 'now'))
+            ->setEventEndDate($endDate);
+
             $manager->persist($event);
         }
 
